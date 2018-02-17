6596e0dadf5c82695a600d8d895dce50d540bdb8
Dave Ansell <dave@sciansell.co.uk>
2018-02-17 12:42:37 +0000

    convert to using wave module from audiotools

--- audio.py
+++ audio.py
@@ -1,26 +1,38 @@
 
 from PIL import Image
 from PIL import ImageDraw
-import audiotools
+import wave, struct
 #f = "/usr/share/kivy-examples/audio/12927_sweet_trip_mm_sweep_z.wav"
 #f = '/home/dave/cnc/software/media/audio2/cochlear/clean/classical.wav.mp3'
 #f='/home/dave/cnc/software/media/audio2/ageing/rock.wav.mp3'
 f='rock.wav'
-pcm_data = audiotools.open(f).to_pcm()
-width = 20000 #pcm_data.frames
+pcm_data = wave.open(f)
+width = 1200*30 #pcm_data.frames
 height = 100
-framelist = pcm_data.read(20000).to_float()
+nchannels, sampwidth, framerate, nframes, comptype, compname = pcm_data.getparams()
+
+width = min(width, nframes)
+print width
+#width = 20
+
+framelist = pcm_data.readframes(width)
+
+#print framelist
 
 img = Image.new('RGB', (width, height))
 draw = ImageDraw.Draw(img)
 m = 0
-for i in range(0, len(framelist)):
-	m = max(m, framelist[i])
+data=[]
+for i in range(0, width):
+	data.append(struct.unpack("<h",  pcm_data.readframes(1)))
+#	data.append( int(struct.unpack("<h",  pcm_data.readframes(1)[0])))
+	m = max(m, data[i][0])
+print m
 scale= 1.0/m
-
-for i in range(0, len(framelist)):
-	col=int((framelist[i]*scale+1)*128)
+print scale
+for i in range(0, width):
+	col=int((data[i][0]*scale+1)*128)
 	draw.line([(i,0), (i,100)], fill=(col,col,col))
-	print( (framelist[i]*3+1)*128)
+	print( (data[i][0]*3+1)*128)
 
 img.save("test.png")
